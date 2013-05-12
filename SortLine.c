dedcedfcba9617abfcb4a592d8ddf5c6bd8fd66d
bananalobster <nichka140@gmail.com>
2013-05-12 14:21:13 +0300

    Update SortLine.c

--- SortLine.c
+++ SortLine.c
@@ -1,22 +1,43 @@
 #include <stdio.h>
 
-void main(){
-  char tmp;
-	int i, j, am, e = 1, counter = 0;
-	char a[] = "retsbol";
+#define	N 255
 
-	for (j = 0; j <= sizeof(a); j++){
-		for (i = 0; i <= sizeof(a); i++){
-			counter++;
-			if (e*a[i+1] < e*a[i]){
+int main(){
+	char tmp;
+	int i, fl, am, e = 1, len = 0;
+	char a[N];
+
+	/* input */
+	scanf("%s", a);
+
+	/* length */
+	do {
+		len++;
+	} while (a[len]*e > 13);
+
+	/* check input */
+//	for (i = 0; i <= len; i++){
+//		printf("%c", a[i]);
+//	}
+
+	/* sort */
+	do{
+		fl = 0;
+		for (i = 0; i < len-1; i++){
+			if  (e*a[i+1] < e*a[i]){
+				fl = 1;
 				tmp = a[i];
 				a[i] = a[i + 1];
 				a[i + 1] = tmp;
 			}
 		}	
+	}while (fl != 0);
+
+	/* output */
+	for (i = 0; i <= len; i++){
+		printf("%c", a[i]);
 	}
-	for (i = 0; i <= sizeof(a); i++){
-		printf("%c",a[i]);
-	}
-	printf("\n %d\n", counter);
+	printf("\n");
+
+	return 0;
 }
